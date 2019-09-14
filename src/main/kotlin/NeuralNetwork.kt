a83daecbb91ded401a02838fee132e34496297ae
Mikhail Meleshkin <mikhail.meleshkin@gmail.com>
2019-09-14 20:25:46 +0300

    Added train method

--- src/main/kotlin/NeuralNetwork.kt
+++ src/main/kotlin/NeuralNetwork.kt
@@ -1,9 +1,9 @@
 import org.apache.commons.math3.linear.RealMatrix
 
 class NeuralNetwork (nInput: Int, nHidden: Int, nOutput: Int,
-                     val learningRate: Int) {
-    private val weightInputHidden = createRandom(nHidden, nInput)
-    private val weightHiddenOutput = createRandom(nOutput, nHidden)
+                     private val learningRate: Double) {
+    private var weightInputHidden = createRandom(nHidden, nInput)
+    private var weightHiddenOutput = createRandom(nOutput, nHidden)
 
 
     fun query(vararg inputValues: Double): RealMatrix {
@@ -31,19 +31,35 @@ class NeuralNetwork (nInput: Int, nHidden: Int, nOutput: Int,
         val outputError = target.subtract(finalOutputs)
         val hiddenError = weightHiddenOutput.transpose().multiply(outputError)
 
+        val weightHiddenOutputDelta =
+                outputError.scalarMultiple(finalOutputs)!!
+                    .scalarMultiple(finalOutputs.scalarSubtractionFrom(1.0))!!
+                    .multiply(hiddenOutputs.transpose())!!
+                    .scalarMultiply(learningRate)
+
+        weightHiddenOutput = weightHiddenOutput.add(weightHiddenOutputDelta)
+
+        val weightInputHiddenDelta =
+                hiddenError.scalarMultiple(hiddenOutputs)!!
+                    .scalarMultiple(hiddenOutputs.scalarSubtractionFrom(1.0))!!
+                    .multiply(input.transpose())!!
+                    .scalarMultiply(learningRate)
+
+        weightInputHidden = weightInputHidden.add(weightInputHiddenDelta)
 
     }
 }
 
 fun main() {
+
     val nInput = 3
     val nHidden = 3
     val nOutput = 3
-
-    val lRate = 2
+    val lRate = 0.3
 
     val net = NeuralNetwork(nInput, nHidden, nOutput, lRate)
 
-    val res = net.query(1.0, 2.0, 3.0)
-    print(res)
+    net.train(doubleArrayOf(1.0, 2.0, 3.0), doubleArrayOf(1.0, 2.0, 3.0))
+
+
 }
