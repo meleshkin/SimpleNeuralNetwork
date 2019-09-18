56c6d6a2ba7bec228d68bd92c01b7ede40fafacd
Mikhail Meleshkin <mikhail.meleshkin@gmail.com>
2019-09-18 21:29:40 +0300

    Added impl. of query method

--- src/main/kotlin/NeuralNetwork.kt
+++ src/main/kotlin/NeuralNetwork.kt
@@ -1,4 +1,6 @@
 import org.apache.commons.math3.linear.RealMatrix
+import java.nio.file.Files
+import java.nio.file.Paths
 
 class NeuralNetwork (nInput: Int, nHidden: Int, nOutput: Int,
                      private val learningRate: Double) {
@@ -6,7 +8,7 @@ class NeuralNetwork (nInput: Int, nHidden: Int, nOutput: Int,
     private var weightHiddenOutput = createRandom(nOutput, nHidden)
 
 
-    fun query(vararg inputValues: Double): RealMatrix {
+    fun query(inputValues: DoubleArray): RealMatrix {
         val input = createColumn(inputValues)
 
         val hiddenInputs = weightInputHidden.multiply(input)
@@ -51,15 +53,26 @@ class NeuralNetwork (nInput: Int, nHidden: Int, nOutput: Int,
 }
 
 fun main() {
-
-    val nInput = 3
-    val nHidden = 3
-    val nOutput = 3
+    val nInput = 784
+    val nHidden = 100
+    val nOutput = 10
     val lRate = 0.3
 
     val net = NeuralNetwork(nInput, nHidden, nOutput, lRate)
 
-    net.train(doubleArrayOf(1.0, 2.0, 3.0), doubleArrayOf(1.0, 2.0, 3.0))
+    val lines = Files.readAllLines(Paths.get("resources/mnist_train_100.csv"))
+    lines.forEach {
 
+        val digit = targetDigit(it).toInt()
+        val targets = scaledTargetsFromCsvString(nOutput, digit)
+        val inputs = createColumn(scaledInputsFromCsvString(it).toDoubleArray())
 
+        net.train(inputs.getColumn(0), targets)
+    }
+
+    val testString = "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,11,150,253,202,31,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,37,251,251,253,107,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,21,197,251,251,253,107,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,110,190,251,251,251,253,169,109,62,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,253,251,251,251,251,253,251,251,220,51,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,182,255,253,253,253,253,234,222,253,253,253,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,63,221,253,251,251,251,147,77,62,128,251,251,105,0,0,0,0,0,0,0,0,0,0,0,0,0,0,32,231,251,253,251,220,137,10,0,0,31,230,251,243,113,5,0,0,0,0,0,0,0,0,0,0,0,0,37,251,251,253,188,20,0,0,0,0,0,109,251,253,251,35,0,0,0,0,0,0,0,0,0,0,0,0,37,251,251,201,30,0,0,0,0,0,0,31,200,253,251,35,0,0,0,0,0,0,0,0,0,0,0,0,37,253,253,0,0,0,0,0,0,0,0,32,202,255,253,164,0,0,0,0,0,0,0,0,0,0,0,0,140,251,251,0,0,0,0,0,0,0,0,109,251,253,251,35,0,0,0,0,0,0,0,0,0,0,0,0,217,251,251,0,0,0,0,0,0,21,63,231,251,253,230,30,0,0,0,0,0,0,0,0,0,0,0,0,217,251,251,0,0,0,0,0,0,144,251,251,251,221,61,0,0,0,0,0,0,0,0,0,0,0,0,0,217,251,251,0,0,0,0,0,182,221,251,251,251,180,0,0,0,0,0,0,0,0,0,0,0,0,0,0,218,253,253,73,73,228,253,253,255,253,253,253,253,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,113,251,251,253,251,251,251,251,253,251,251,251,147,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,31,230,251,253,251,251,251,251,253,230,189,35,10,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,62,142,253,251,251,251,251,253,107,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,72,174,251,173,71,72,30,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0"
+    val input = createColumn(scaledInputsFromCsvString(testString).toDoubleArray()).getColumn(0)
+    val resultArray = net.query(input)
+    val result = resultArray.getColumn(0).indexOf(resultArray.getColumn(0).max()!!)
+    print(result)
 }
--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -1,5 +1,10 @@
 import org.apache.commons.math3.linear.MatrixUtils
 import org.apache.commons.math3.linear.RealMatrix
+import java.awt.image.BufferedImage
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
+import javax.imageio.ImageIO
 import kotlin.math.exp
 import kotlin.random.Random
 
@@ -19,11 +24,10 @@ fun createRandom(nRows: Int, nColumns: Int): RealMatrix {
     val m = MatrixUtils.createRealMatrix(nRows, nColumns)
     for (row in 0 until nRows)
         for (column in 0 until nColumns)
-            m.setEntry(row, column, rnd.nextDouble()-0.5)
+            m.setEntry(row, column, rnd.nextDouble() - 0.5)
     return m
 }
 
-//fun createColumn(vararg column: Double) = MatrixUtils.createColumnRealMatrix(column)
 fun createColumn(column: DoubleArray) = MatrixUtils.createColumnRealMatrix(column)
 
 fun RealMatrix.scalarMultiple(other: RealMatrix): RealMatrix? {
@@ -43,7 +47,60 @@ fun RealMatrix.scalarSubtractionFrom(x: Double): RealMatrix {
     val result = MatrixUtils.createRealMatrix(rowDimension, columnDimension)
     for (row in 0 until rowDimension)
         for (column in 0 until columnDimension)
-            result.setEntry(row, column,
-                    x - getEntry(row, column))
+            result.setEntry(row, column, x - getEntry(row, column))
     return result
+}
+
+
+fun mnistCsvToJpg(inputFilePath: String, outputFilePath: String, index: Int) {
+    val image = BufferedImage(28, 28, BufferedImage.TYPE_BYTE_GRAY)
+    val lines = Files.readAllLines(Paths.get(inputFilePath))
+    var pixels = lines[index].split(",")
+    pixels = pixels.subList(1, pixels.size)
+    var column: Int
+    var row = -1
+
+    for (i in 0 until pixels.size) {
+        column = i % 28
+        if (column == 0) {
+            row++
+        }
+        var colorValue = pixels[i].toInt()
+        if (colorValue == 0) {
+            colorValue = Int.MAX_VALUE
+        }
+        image.setRGB(column, row, colorValue)
+    }
+
+    ImageIO.write(image, "jpg", File(outputFilePath))
+}
+
+
+fun scaledInputsFromCsvString(csvStr: String): ArrayList<Double> {
+    val scaledList = ArrayList<Double>()
+    var pixels = csvStr.split(",")
+    for (i in 1 until pixels.size) {
+        scaledList.add( (pixels[i].toDouble() / 255.0 * 0.99) + 0.1)
+    }
+    return scaledList
+}
+
+
+fun scaledTargetsFromCsvString(nOutputs: Int, targetDigit: Int): DoubleArray {
+    val tempList = List(nOutputs) {0.01}
+    val targets = tempList.toDoubleArray()
+    targets[targetDigit] = 0.99
+    return targets
+}
+
+fun targetDigit(csvStr: String): String {
+    var pixels = csvStr.split(",")
+    return pixels[0]
+}
+
+fun main() {
+    val inputFile = "resources/mnist_train_100.csv"
+    val outputFile = "resources/mnist_train_100.jpg"
+    val index = 1
+    mnistCsvToJpg(inputFile, outputFile, index)
 }
